a7663e6252b61597317de1518bee46fb5e286d9d
Urvish2007 <urvishbabariya06@gmail.com>
2026-06-04 07:46:48 +0530

    Add project folders and initial setup

---
+++ Week-0/API call/Hello.py
@@ -0,0 +1,20 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY1"))
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{
+        "role": "user",
+        "content": "What is Generative AI in exactly 2 sentences?"
+    },{
+         "role" : "user",
+        "content": "Explain difference array algorithm"
+    }]
+)
+
+print(response.choices[0].message.content)
